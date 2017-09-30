cd382501ded7247b055cb73f47822e38da20733d
metr1ckzu <metr1ckzu@gmail.com>
2017-09-30 20:49:37 +0600

    simpleslider

--- src/components/organisms/SliderDraft/index.js
+++ src/components/organisms/SliderDraft/index.js
@@ -1,68 +1,26 @@
 import React from 'react'
+import Slider from 'react-slick'
 
-const style = {
-  header: {
-    backgroundColor: 'transparent',
-    height:'300px',
-    alignItems: 'center',
-  },
-  text: {
-    flex:'1 0 auto',
-    textAlign: 'center'
-  },
-  content: {
-    margin: '0 auto',
-    border: 'solid 1px #111',
-    height: '300px',
-    width: '1500px',
-    display: '-ms-flexbox',
-    display: '-webkit-flex',
-    display: 'flex',
-  },
-  card: {
-    border: 'solid 1px #111',
-    maxHeight: '340px',
-    width: '300px',
-    margin: '8px',
-    textAlign: 'center'
-  }
-}
+var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
 
-const Slider = ({onClickNext, onClickPrev}) => {
+const SimpleSlider = ({settings}) => {
   return (
-    <div style={style.header}>
-      <div style={style.text}>
-        <h2>Популярное</h2>
-      </div>
-      <div style={style.content}>
-        <div onClick={e => {
-          e.preventDefault()
-          onClickPrev()
-        }}>{'<'}</div>
-        <div style={style.card}>
-          <p>Популярное</p>
-        </div>
-        <div style={style.card}>
-          <p>Популярное</p>
-        </div>
-        <div style={style.card}>
-          <p>Популярное</p>
-        </div>
-        <div style={style.card}>
-          <p>Популярное</p>
-        </div>
-        <div style={style.card}>
-          <p>Популярное</p>
-        </div>
-        <div onClick={e =>{
-          e.preventDefault()
-          onClickPrev()
-        }}>{'>'}</div>
-      </div>
-    </div>
+    <Slider {...settings}>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+        <div><h3>5</h3></div>
+        <div><h3>6</h3></div>
+      </Slider>
+    );
   )
 }
 
-Slider.propTypes = {}
-
-export default Slider
+export default SimpleSlider
