b75b4a643016ec6e26c816117c4842e3b2fbc832
metr1ckzu <metr1ckzu@gmail.com>
2017-10-01 07:49:03 +0600

    added other container mocks

--- src/containers/PhotoVideo.js
+++ src/containers/PhotoVideo.js
@@ -3,7 +3,34 @@ import { fetchPhotoVideoRequest } from 'store/actions'
 import { PhotoVideo } from 'components'
 
 export const mapStateToProps = state => ({
-  photoVideo: state.photoVideo
+  // photoVideo: state.photoVideo
+  photoVideo:
+  [
+      {
+          "id": 1,
+          "name": "Ала-Тооd",
+          "capacity": "500",
+          "sum": 3,
+          "kitchen": 2,
+          "additional_service": 6,
+          "description": "У нас крутой ресторан",
+          "address": "г.Бишкек Асанбай",
+          "phone": "+996550778133",
+          "photo": "http://localhost:8000/media/photos/restaurant/JUu2nuYUtlk.jpg"
+      },
+      {
+          "id": 2,
+          "name": "Нава",
+          "capacity": "100",
+          "sum": 1,
+          "kitchen": 1,
+          "additional_service": 4,
+          "description": "Супер ресторан",
+          "address": "г.Бишкек",
+          "phone": "+996558728259",
+          "photo": "http://localhost:8000/media/photos/restaurant/JUu2nuYUtlk_nC8xlxQ.jpg"
+      }
+  ]
 })
 
 export const mapDispatchToProps = (dispatch) => ({
--- src/containers/ToastMasters.js
+++ src/containers/ToastMasters.js
@@ -3,7 +3,33 @@ import { fetchToastMastersRequest } from 'store/actions'
 import { ToastMasters } from 'components'
 
 export const mapStateToProps = state => ({
-  toastMasters: state.toastMaster
+  // toastMasters: state.toastMaster
+  toastMasters: [
+      {
+          "id": 1,
+          "name": "Ала-Тооd",
+          "capacity": "500",
+          "sum": 3,
+          "kitchen": 2,
+          "additional_service": 6,
+          "description": "У нас крутой ресторан",
+          "address": "г.Бишкек Асанбай",
+          "phone": "+996550778133",
+          "photo": "http://localhost:8000/media/photos/restaurant/JUu2nuYUtlk.jpg"
+      },
+      {
+          "id": 2,
+          "name": "Нава",
+          "capacity": "100",
+          "sum": 1,
+          "kitchen": 1,
+          "additional_service": 4,
+          "description": "Супер ресторан",
+          "address": "г.Бишкек",
+          "phone": "+996558728259",
+          "photo": "http://localhost:8000/media/photos/restaurant/JUu2nuYUtlk_nC8xlxQ.jpg"
+      }
+  ]
 })
 
 export const mapDispatchToProps = (dispatch) => ({
