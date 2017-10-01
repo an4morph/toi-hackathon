082beb9c2f240a8f1e9a2e63de8b6cb5109de794
metr1ckzu <metr1ckzu@gmail.com>
2017-10-01 08:51:03 +0600

    showprogram container store mock

--- src/containers/ShowProgram.js
+++ src/containers/ShowProgram.js
@@ -6,30 +6,16 @@ export const mapStateToProps = state => ({
   // showProgram: state.showProgram
   showProgram:
   [
-      {
-          "id": 1,
-          "name": "Ала-Тооd",
-          "capacity": "500",
-          "sum": 3,
-          "kitchen": 2,
-          "additional_service": 6,
-          "description": "У нас крутой ресторан",
-          "address": "г.Бишкек Асанбай",
-          "phone": "+996550778133",
-          "photo": "http://localhost:8000/media/photos/restaurant/JUu2nuYUtlk.jpg"
-      },
-      {
-          "id": 2,
-          "name": "Нава",
-          "capacity": "100",
-          "sum": 1,
-          "kitchen": 1,
-          "additional_service": 4,
-          "description": "Супер ресторан",
-          "address": "г.Бишкек",
-          "phone": "+996558728259",
-          "photo": "http://localhost:8000/media/photos/restaurant/JUu2nuYUtlk_nC8xlxQ.jpg"
-      }
+    {
+        "id": 1,
+        "name": "Макс Бавлянов",
+        price: "600",
+        "description": "Lorem Ipsum - это текст-'рыба', часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.",
+        "address": "г.Бишкек Асанбай",
+        "phone": "+996550778133",
+        "photo": "photographer1.jps"
+    },
+
   ]
 })
 
