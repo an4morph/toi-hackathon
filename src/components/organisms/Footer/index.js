0e634c2921a451a4ad27a5463a72dcea0c6786a0
metr1ckzu <metr1ckzu@gmail.com>
2017-10-01 12:39:21 +0600

    changed footer

--- src/components/organisms/Footer/index.js
+++ src/components/organisms/Footer/index.js
@@ -41,34 +41,16 @@ const Service = () => {
         <div className='container'>
           <div className='row'>
           <div className='numbers col-lg-4'>
-            <p>Keker +4616135487181641</p>
-            <p>Keker man +4616135487181641</p>
-            <p>Keker girl+4616135487181641</p>
-            <p>Keker dev+4616135487181641</p>
-            <p>Keker lol kek+4616135487181641</p>
-            <p>Ketrin kek+4616135487181641</p>
-            <p>Loler kek+4616135487181641</p>
-            <p>Keker Keker+4616135487181641</p>
+            <p>О Toimaster</p>
+            <p>Сотрудничество</p>
           </div>
           <div className='map col-lg-4'>
-            <a href='/'>kekkekekek</a>
-            <a href='/'>kekkekekekke</a>
-            <a href='/'>kekkekekekkekekek</a>
-            <a href='/'>kekkekekekekke</a>
-            <a href='/'>kekkekekekekkekekek</a>
-            <a href='/'>kekekekekkekekk</a>
-            <a href='/'>kkekekkekekek</a>
-            <a href='/'>kekekekekkekekekkekek</a>
+            <a href='/'>Главная</a>
+            <a href='/construct'>Конструктор</a>
           </div>
           <div className='numbers col-lg-4'>
-            <p>Keker +4616135487181641</p>
-            <p>Keker man +4616135487181641</p>
-            <p>Keker girl+4616135487181641</p>
-            <p>Keker dev+4616135487181641</p>
-            <p>Keker lol kek+4616135487181641</p>
-            <p>Ketrin kek+4616135487181641</p>
-            <p>Loler kek+4616135487181641</p>
-            <p>Keker Keker+4616135487181641</p>
+            <p>Контакты</p>
+            <p>0555872497</p>
           </div>
         </div>
       </div>
@@ -79,3 +61,28 @@ const Service = () => {
 Service.propTypes = {}
 
 export default Service
+
+{/* <p>Keker man +4616135487181641</p>
+<p>Keker girl+4616135487181641</p>
+<p>Keker dev+4616135487181641</p>
+<p>Keker lol kek+4616135487181641</p>
+<p>Ketrin kek+4616135487181641</p>
+<p>Loler kek+4616135487181641</p>
+<p>Keker Keker+4616135487181641</p>
+
+
+<a href='/'>kekkekekekke</a>
+<a href='/'>kekkekekekkekekek</a>
+<a href='/'>kekkekekekekke</a>
+<a href='/'>kekkekekekekkekekek</a>
+<a href='/'>kekekekekkekekk</a>
+<a href='/'>kkekekkekekek</a>
+<a href='/'>kekekekekkekekekkekek</a>
+
+<p>Keker man +4616135487181641</p>
+<p>Keker girl+4616135487181641</p>
+<p>Keker dev+4616135487181641</p>
+<p>Keker lol kek+4616135487181641</p>
+<p>Ketrin kek+4616135487181641</p>
+<p>Loler kek+4616135487181641</p>
+<p>Keker Keker+4616135487181641</p> */}
